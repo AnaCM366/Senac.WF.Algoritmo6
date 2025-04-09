5d51d728325a618a2a5b9c8e19bac3146efb025f
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-08 21:51:03 -0300

    Criação da tela principal

---
+++ SolucaoAlgoritmo6/Algoritmo6/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Algoritmo6
+{
+    partial class Formulario
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label8 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            btnSair = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(255, 192, 192);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI", 10F);
+            groupBox1.Location = new Point(29, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 214);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(186, 166);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(574, 34);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(186, 124);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(574, 34);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(186, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(574, 34);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(186, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(574, 34);
+            textBox1.TabIndex = 4;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(17, 172);
+            label8.Name = "label8";
+            label8.Size = new Size(65, 28);
+            label8.TabIndex = 3;
+            label8.Text = "label8";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 28);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 28);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 28);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.FromArgb(128, 255, 128);
+            groupBox2.Controls.Add(textBox8);
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Controls.Add(textBox6);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Font = new Font("Segoe UI", 10F);
+            groupBox2.Location = new Point(29, 241);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(776, 218);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(186, 167);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(574, 34);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(186, 122);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(574, 34);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(186, 73);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(574, 34);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(186, 24);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(574, 34);
+            textBox5.TabIndex = 4;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(17, 178);
+            label7.Name = "label7";
+            label7.Size = new Size(65, 28);
+            label7.TabIndex = 3;
+            label7.Text = "label7";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(20, 133);
+            label6.Name = "label6";
+            label6.Size = new Size(65, 28);
+            label6.TabIndex = 2;
+            label6.Text = "label6";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(20, 88);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 28);
+            label5.TabIndex = 1;
+            label5.Text = "label5";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 40);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 28);
+            label4.TabIndex = 0;
+            label4.Text = "label4";
+            // 
+            // btnSair
+            // 
+            btnSair.BackColor = Color.FromArgb(255, 255, 192);
+            btnSair.FlatAppearance.BorderSize = 0;
+            btnSair.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 128, 128);
+            btnSair.FlatStyle = FlatStyle.Flat;
+            btnSair.Font = new Font("Segoe UI", 18F);
+            btnSair.Location = new Point(618, 478);
+            btnSair.Name = "btnSair";
+            btnSair.Size = new Size(187, 53);
+            btnSair.TabIndex = 2;
+            btnSair.Text = "Sair";
+            btnSair.TextImageRelation = TextImageRelation.TextBeforeImage;
+            btnSair.UseVisualStyleBackColor = false;
+            btnSair.Click += btnSair_Click;
+            // 
+            // Formulario
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
+            ClientSize = new Size(826, 555);
+            Controls.Add(btnSair);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Formulario";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Formulario";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private GroupBox groupBox2;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label8;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Button btnSair;
+    }
+}
---
+++ SolucaoAlgoritmo6/Algoritmo6/Form1.cs
@@ -0,0 +1,15 @@
+namespace Algoritmo6
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
